2569bf134106c22e72d3a7ea8cc10172fbb3eeff
Mostafa2291 <mostafazayed2007@gmail.com>
2026-03-06 20:07:50 +0200

    Fixed minor issue in invalidCSVFormat class

--- src/game/engine/exceptions/InvalidCSVFormat.java
+++ src/game/engine/exceptions/InvalidCSVFormat.java
@@ -6,27 +6,29 @@ public class InvalidCSVFormat extends IOException {
 
     private static final String MSG="Invalid input detected while reading csv file, input = \n";
    
-    //read write
+    //read & write
     private String inputLine;
    
+
+
+    public InvalidCSVFormat(String inputLine) {
+        super(MSG + inputLine);
+        this.inputLine = inputLine;
+    }
+
+    public InvalidCSVFormat(String message, String inputLine) {
+        super(message + inputLine);
+        this.inputLine = inputLine; 
+    }
+
+
+
     public void setInputLine(String inputLine) {
         this.inputLine = inputLine;
     }
 
-   
-   
     public String getInputLine() {
         return inputLine;
-    }
-
-   
-    public InvalidCSVFormat(String inputLine) {
-        super(MSG + inputLine);
-    }
-
-   
-    public InvalidCSVFormat(String message, String inputLine) {
-        super(message + inputLine);
-    }
+    }    
 
 }
